24a1be713584448341adf791be1e5f2e5bfd7d4e
Juandeyby <deyby57@gmail.com>
2019-11-19 16:18:29 -0500

    First version all places hotel
    all places of the hotel villa elisa

---
+++ TourismVillaElisa/Assets/Scripts/MainController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.VR;
+
+public class MainController : MainElement
+{
+    public GameObject parentCamera;
+
+    public GameObject firstPoint;
+    private Vector3 rotateOld = new Vector3 (0, 90, 0);
+
+    [HideInInspector]
+    public GameObject activeSelected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //RenderTexture backupRenderTexture = RenderTexture.active;
+        //backupRenderTexture.DiscardContents();
+        ChangeTexture(firstPoint, null);
+        mainApp.mainController.parentCamera.transform.position = new Vector3(firstPoint.transform.position.x, 0, firstPoint.transform.position.z);
+    }
+
+    public void ChangeTexture(GameObject point, GameObject oldpoint)
+    {
+        //
+        //var fileName = Application.persistentDataPath + "/" + point.gameObject.name + ".jpg";
+        //var bytes = File.ReadAllBytes(fileName);
+        //var texture = new Texture2D(6080/2, 3040/2, TextureFormat.RGBA32, false);
+        //texture.LoadImage(bytes);
+        //
+        //mainApp.mainView.photoApartment.GetComponent<Renderer>().material.mainTexture = texture;
+        mainApp.mainView.photoApartment.GetComponent<Renderer>().material.mainTexture = Resources.Load<Texture2D>("Textures/" + point.gameObject.name);
+        mainApp.mainView.photoApartment.transform.position =
+            new Vector3(point.transform.position.x, mainApp.mainView.photoApartment.transform.position.y, point.transform.position.z);
+        mainApp.mainController.parentCamera.transform.position = new Vector3(point.transform.position.x, 0, point.transform.position.z);
+        //Rotate
+        mainApp.mainView.photoApartment.transform.Rotate(- rotateOld);
+        mainApp.mainView.photoApartment.transform.Rotate(point.GetComponent<ObjectJumping>().rotate);
+        rotateOld = point.GetComponent<ObjectJumping>().rotate;
+
+        if (oldpoint != null)
+            oldpoint.gameObject.SetActive(false);
+
+        //Get points
+        //for (int i = 0; i < mainApp.mainModel.points.Count; i++)
+        //{
+        //    mainApp.mainModel.points[i].gameObject.SetActive(false);
+        //}
+        //for (int i = 0; i < point.GetComponent<ObjectJumping>().neighbors.Count; i++)
+        //{
+        //    point.GetComponent<ObjectJumping>().neighbors[i].gameObject.SetActive(true);
+        //}
+        //point.transform.gameObject.SetActive(false);
+    }
+
+    private RenderTexture CreateNewRenderTexture(int width, int height)
+    {
+        RenderTexture renderTexture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+        renderTexture.filterMode = FilterMode.Point;
+        renderTexture.DiscardContents();
+
+        return renderTexture;
+    }
+}
---
+++ TourismVillaElisa/Assets/Scripts/ObjectJumping.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ObjectJumping : MainElement
+{
+    public Vector3 rotate;
+    private bool activePoint = true;
+    private Renderer myRenderer;
+
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+
+    public void OnEnter(GameObject thisObject)
+    {
+        myRenderer = thisObject.GetComponent<Renderer>();
+        myRenderer.material = gazedAtMaterial;
+        AudioSource.PlayClipAtPoint(Resources.Load<AudioClip>("Sounds/Touch"), mainApp.mainController.parentCamera.transform.position, 0.5f);
+    }
+
+    public void OnExit(GameObject thisObject)
+    {
+        myRenderer = thisObject.GetComponent<Renderer>();
+        myRenderer.material = inactiveMaterial;
+    }
+
+    public void TransportCamera(GameObject point)
+    {
+        if (activePoint)
+        {
+            activePoint = false;
+            point.SetActive(true);
+            AudioSource.PlayClipAtPoint(Resources.Load<AudioClip>("Sounds/TouchDown"), mainApp.mainController.parentCamera.transform.position);
+            StartCoroutine(ChangePoint(point));
+        }
+    }
+
+    IEnumerator ChangePoint(GameObject point)
+    {
+        mainApp.mainController.parentCamera.transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+        mainApp.mainController.ChangeTexture(point, gameObject);
+        activePoint = true;
+
+        yield return null;
+
+        //while (true)
+        //{
+        //    mainApp.mainController.parentCamera.transform.position = Vector3.Lerp(
+        //        mainApp.mainController.parentCamera.transform.position,
+        //        new Vector3(transform.position.x, 0, transform.position.z
+        //        ), Time.deltaTime * 2);
+        //    if (Mathf.RoundToInt(mainApp.mainController.parentCamera.transform.position.x) == transform.position.x &&
+        //        Mathf.RoundToInt(mainApp.mainController.parentCamera.transform.position.z) == transform.position.z)
+        //    {
+        //        mainApp.mainController.ChangeTexture(this.gameObject);
+        //        break;
+        //    }
+        //    yield return new WaitForSeconds(Time.deltaTime);
+        //}
+    }
+}
